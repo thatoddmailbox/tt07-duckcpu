4ecf21ca985ac94bd88e32c4658e8d1ffbda8c18
Alex Studer <alex@studer.dev>
2024-05-30 23:45:49 -0400

    project: remove sample adder

--- src/project.v
+++ src/project.v
@@ -17,7 +17,7 @@ module tt_um_thatoddmailbox (
 );
 
 	// All output pins must be assigned. If not used, assign to 0.
-	assign uo_out[3:0] = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+	assign uo_out[3:0] = 4'h0; //ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
 	assign uio_out = 0;
 	assign uio_oe  = 0;
 
