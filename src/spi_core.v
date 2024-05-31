f414fbdf24b43e0cb16b3c0dab010f847f341df4
Alex Studer <alex@studer.dev>
2024-05-30 22:19:25 -0400

    spi_core: allow forcing a single clock cycle

--- src/spi_core.v
+++ src/spi_core.v
@@ -13,7 +13,8 @@ module spi_core(
 	input wire [7:0] data_tx,
 	output reg [7:0] data_rx,
 	input wire txn_start,
-	output wire txn_done
+	output wire txn_done,
+	input wire force_clock
 );
 
 	reg [4:0] counter;
@@ -22,6 +23,8 @@ module spi_core(
 	reg active;
 	reg [2:0] bit_count;
 
+	reg forcing_clock;
+
 	assign txn_done = !active;
 
 	always @(posedge clk) begin
@@ -29,6 +32,8 @@ module spi_core(
 			tx_buf <= 8'h00;
 			active <= 1'b0;
 			bit_count <= 3'b0;
+
+			forcing_clock <= 1'b0;
 
 			counter <= 0;
 
@@ -42,6 +47,9 @@ module spi_core(
 					tx_buf <= data_tx;
 					active <= 1'b1;
 					bit_count <= 3'b0;
+				end else if (force_clock) begin
+					active <= 1'b1;
+					forcing_clock <= 1'b1;
 				end
 			end else begin
 				counter <= counter + 1;
@@ -49,17 +57,26 @@ module spi_core(
 				if (counter == divider) begin
 					spi_clk <= ~spi_clk;
 					counter <= 0;
-					if (spi_clk == 1'b0) begin
-						// we just made the clk go up, so we should shift out the next bit
-						// TODO: cpha bit???
-						tx_buf <= {tx_buf[6:0], 1'b0};
-						spi_mosi <= tx_buf[7];
-						bit_count <= bit_count + 1;
-					end else begin
-						// we just made the clk go down, so we should read in the next bit
-						data_rx <= {data_rx[6:0], spi_miso};
-						if (bit_count == 3'h0) begin
+
+					if (forcing_clock) begin
+						if (spi_clk) begin
+							// we did it, go back to normal
 							active <= 1'b0;
+							forcing_clock <= 1'b0;
+						end
+					end else begin
+						if (spi_clk == 1'b0) begin
+							// we just made the clk go up, so we should shift out the next bit
+							// TODO: cpha bit???
+							tx_buf <= {tx_buf[6:0], 1'b0};
+							spi_mosi <= tx_buf[7];
+							bit_count <= bit_count + 1;
+						end else begin
+							// we just made the clk go down, so we should read in the next bit
+							data_rx <= {data_rx[6:0], spi_miso};
+							if (bit_count == 3'h0) begin
+								active <= 1'b0;
+							end
 						end
 					end
 				end
